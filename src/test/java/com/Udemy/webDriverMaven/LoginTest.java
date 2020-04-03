376f5fea299c9a8d561740ebf77c32b025fe5392
AnujGupta58 <58.anujgupta@gmail.com>
2020-04-03 23:07:32 +0530

    first commit

---
+++ src/test/java/com/Udemy/webDriverMaven/LoginTest.java
@@ -0,0 +1,36 @@
+package com.Udemy.webDriverMaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "F:\\\\software\\\\jar_files\\\\chromedriver_win32\\\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5L, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void search() {
+		//driver.get("http://google.com/");
+		driver.findElement(By.name("q")).sendKeys("Testing");
+		driver.findElement(By.className("gNO89b")).click();
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.close();
+	}
+}
